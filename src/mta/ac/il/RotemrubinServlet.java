545b6bb894eeaf9ebceaeb90b31b5508eadb391d
rotemru <rotemru@M210WS28A.mta.ac.il>
2014-11-10 13:56:43 +0200

    commit1

---
+++ src/mta/ac/il/RotemrubinServlet.java
@@ -0,0 +1,20 @@
+package mta.ac.il;
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class RotemrubinServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/html");
+		int num1, num2, num3;
+		num1=4;
+		num2=3;
+		num3=7;
+		
+		int result=(num1+num2)*num3;
+		
+		String resultStr = new String("<h1>Result of: (" +num1+"+"+num2+ ")*"+num3+"="+result+"<h1>");
+		
+		resp.getWriter().println(resultStr);
+	}
+}
