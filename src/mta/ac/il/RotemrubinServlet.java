504fa4599d25a1527080f717a674ed31d1e971b4
rotem5490 <rotem1rub@gmail.com>
2014-11-17 12:59:46 +0200

    exc 3 H.W

--- src/mta/ac/il/RotemrubinServlet.java
+++ src/mta/ac/il/RotemrubinServlet.java
@@ -15,6 +15,25 @@ public class RotemrubinServlet extends HttpServlet {
 		
 		String resultStr = new String("<h1>Result of: (" +num1+"+"+num2+ ")*"+num3+"="+result+"<h1>");
 		
-		resp.getWriter().println(resultStr);
+		double radius = 50;
+		double pai=Math.PI;
+		double area = pai*(radius)*(radius);
+		
+		int angleB=30;
+		int hyp=50;
+		double r=Math.toRadians(angleB);
+		double opp=r*hyp;
+		
+		int base=20;
+		int exp=13;		
+		double res3= Math.pow(base, exp);
+		
+		String line1 = new String("calculation 1: Area of circle with radius " +radius+ " is: " +area+ " square­cm");
+		String line2 = new String("calculation 2: Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: " +opp+ " cm");
+		String line3 = new String("calculation 3: Power of 20 with exp of 13 is: " +res3);
+
+        String resultStr1 = line1 + "<br>" + line2 + "<br>" +line3;
+		
+		resp.getWriter().println(resultStr1);
 	}
 }
